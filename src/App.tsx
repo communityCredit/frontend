13332a4ebf7f74c4b7b5dde4f45ac71721692955
Ankit Kokane <ankitkokane90@gmail.com>
2025-07-05 02:39:31 +0200

    added portfolio page base ui

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import Lend from "./pages/Lend/Deposit";
 import LendLayout from "./pages/Lend/LendLayout";
+import Portfolio from "./pages/Lend/Portfolio";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<LandingPage />} />
         <Route element={<LendLayout />}>
           <Route path="/lend/deposit" element={<Lend />} />
+          <Route path="/lend/portfolio" element={<Portfolio />} />
         </Route>
       </Routes>
     </BrowserRouter>
